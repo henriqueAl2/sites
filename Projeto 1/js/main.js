019dd645ebcd65b65c9f0e7d748d5809d715cdb2
henriqueAl2 <91295739+henriqueAl2@users.noreply.github.com>
2024-06-18 22:28:16 -0300

    Add files via upload

---
+++ Projeto 1/js/main.js
@@ -0,0 +1,37 @@
+
+$('.sliderPrincipal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+  $('.sliderPrincipal1').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+  $('.sliderPrincipal3').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+  $('.sliderPrincipal2').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
